1ef70ea63bbf32e4ca94fc433f09af37f9140874
Gabe Jackson <gabe@apcera.com>
2019-04-10 17:03:46 -0400

    Add support for managing multiple clusters (#2)
    * Add support for managing multiple clusters
    
    This adds initial support for having multiple clusters running
    concurrently. Most of the cluster state data still resides locally
    in directories labelled with the cluster ID.
    
    * Address comments with improvements
    
    * Set terraform output value to interface
    
    * Remove custom cluster ID option

--- internal/provisioner/cluster.go
+++ internal/provisioner/cluster.go
@@ -3,6 +3,7 @@ package provisioner
 import (
 	"fmt"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/mattermost/mattermost-server/model"
@@ -19,6 +20,9 @@ const ProviderAWS = "aws"
 // SizeAlef500 is a cluster sized for 500 users.
 const SizeAlef500 = "SizeAlef500"
 
+// clusterRootDir is the local directory that contains cluster configuration.
+const clusterRootDir = "clusters"
+
 // CreateCluster creates a cluster using kops and terraform.
 func CreateCluster(provider, size string, logger log.FieldLogger) error {
 	provider = strings.ToLower(provider)
@@ -30,15 +34,28 @@ func CreateCluster(provider, size string, logger log.FieldLogger) error {
 		return fmt.Errorf("unsupported size %s", size)
 	}
 
-	outputDir := "tmp"
-	_, err := os.Stat("tmp")
-	if err == nil {
-		return errors.New("tmp folder already exists: delete existing cluster first")
-	} else if err != nil && !os.IsNotExist(err) {
-		return errors.Wrap(err, "failed to stat temporary output directory")
+	clusterId := model.NewId()
+
+	// Temporarily locate the kops output directory to a local folder based on the
+	// cluster name. This won't be necessary once we persist the output to S3 instead.
+	_, err := os.Stat(clusterRootDir)
+	if err != nil && os.IsNotExist(err) {
+		err = os.Mkdir(clusterRootDir, 0755)
+		if err != nil {
+			return errors.Wrap(err, "unable to create cluster root dir")
+		}
+	} else if err != nil {
+		return errors.Wrapf(err, "failed to stat cluster root directory %q", clusterRootDir)
 	}
 
-	clusterId := model.NewId()
+	outputDir := path.Join(clusterRootDir, clusterId)
+	_, err = os.Stat(outputDir)
+	if err == nil {
+		return fmt.Errorf("encountered cluster ID collision: directory %q already exists", outputDir)
+	} else if err != nil && !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to stat cluster directory %q", outputDir)
+	}
+
 	s3StateStore := "dev.cloud.mattermost.com"
 	dns := fmt.Sprintf("%s-kops.k8s.local", clusterId)
 
@@ -56,11 +73,9 @@ func CreateCluster(provider, size string, logger log.FieldLogger) error {
 		return err
 	}
 
-	// Temporarily relocate the kops output directory to a local folder named `tmp`. This won't
-	// be necessary once we persist the output to S3 instead.
 	err = os.Rename(kops.GetOutputDirectory(), outputDir)
 	if err != nil {
-		return errors.Wrap(err, "failed to rename kops output directory to tmp")
+		return fmt.Errorf("failed to rename kops output directory to %q", outputDir)
 	}
 
 	terraform := terraform.New(outputDir, logger)
@@ -84,29 +99,47 @@ func CreateCluster(provider, size string, logger log.FieldLogger) error {
 func DeleteCluster(clusterId string, logger log.FieldLogger) error {
 	logger = logger.WithField("cluster", clusterId)
 
-	logger.Info("deleting cluster")
-
 	s3StateStore := "dev.cloud.mattermost.com"
 	dns := fmt.Sprintf("%s-kops.k8s.local", clusterId)
 
-	// Temporarily look for the kops output directory as a local folder named `tmp`. See above.
-	outputDir := "tmp"
+	// Temporarily look for the kops output directory as a local folder named after
+	// the cluster ID. See above.
+	outputDir := path.Join(clusterRootDir, clusterId)
+
+	// Validate the provided cluster ID before we alter state in any way.
+	_, err := os.Stat(outputDir)
+	if err != nil {
+		return errors.Wrapf(err, "failed to find cluster directory %q", outputDir)
+	}
 
 	terraform := terraform.New(outputDir, logger)
 	defer terraform.Close()
-	err := terraform.Init()
+	err = terraform.Init()
 	if err != nil {
 		return err
 	}
-	err = terraform.Destroy()
+	out, err := terraform.Output("cluster_name")
 	if err != nil {
 		return err
+	}
+	if out != dns {
+		return fmt.Errorf("terraform cluster_name (%s) does not match dns from provided ID (%s)", out, dns)
 	}
 
 	kops, err := kops.New(s3StateStore, logger)
 	defer kops.Close()
 	if err != nil {
 		return errors.Wrap(err, "failed to create kops wrapper")
+	}
+	_, err = kops.GetCluster(dns)
+	if err != nil {
+		return err
+	}
+
+	logger.Info("deleting cluster")
+	err = terraform.Destroy()
+	if err != nil {
+		return err
 	}
 
 	err = kops.DeleteCluster(dns)
--- internal/tools/terraform/plan.go
+++ internal/tools/terraform/plan.go
@@ -1,8 +1,18 @@
 package terraform
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
 	"github.com/pkg/errors"
 )
+
+type terraformOutput struct {
+	Sensitive bool        `json:"sensitive"`
+	Type      string      `json:"type"`
+	Value     interface{} `json:"value"`
+}
 
 // Init invokes terraform init.
 func (c *Cmd) Init() error {
@@ -39,4 +49,35 @@ func (c *Cmd) Destroy() error {
 	}
 
 	return nil
+}
+
+// Output invokes terraform output and returns the value.
+func (c *Cmd) Output(variable string) (string, error) {
+	stdout, _, err := c.run(
+		"output",
+		"-json",
+		variable,
+	)
+	if err != nil {
+		return string(stdout), errors.Wrap(err, "failed to invoke terraform output")
+	}
+
+	var output terraformOutput
+	err = json.Unmarshal(stdout, &output)
+	if err != nil {
+		return string(stdout), errors.Wrap(err, "failed to parse terraform output")
+	}
+
+	return fmt.Sprintf("%s", output.Value), nil
+}
+
+// Version invokes terraform version and returns the value.
+func (c *Cmd) Version() (string, error) {
+	stdout, _, err := c.run("version")
+	trimmed := strings.TrimSuffix(string(stdout), "\n")
+	if err != nil {
+		return trimmed, errors.Wrap(err, "failed to invoke terraform version")
+	}
+
+	return trimmed, nil
 }
