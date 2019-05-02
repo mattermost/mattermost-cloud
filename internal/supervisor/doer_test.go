906274a17f1137b0a923757ef705b310402ffd02
Jesse Hallam <jesse.hallam@gmail.com>
2019-05-02 17:28:39 -0400

    expand doer_test.go

--- internal/supervisor/doer_test.go
+++ internal/supervisor/doer_test.go
@@ -26,24 +26,55 @@ func (fd *failDoer) Do() error {
 }
 
 func TestMultiDoer(t *testing.T) {
-	d1 := &testDoer{calls: make(chan bool, 1)}
-	d2 := &failDoer{}
-	d3 := &testDoer{calls: make(chan bool)}
+	t.Run("failure", func(t *testing.T) {
+		d1 := &testDoer{calls: make(chan bool, 1)}
+		d2 := &failDoer{}
+		d3 := &testDoer{calls: make(chan bool)}
 
-	doer := supervisor.MultiDoer{d1, d2, d3}
+		doer := supervisor.MultiDoer{d1, d2, d3}
 
-	err := doer.Do()
-	require.EqualError(t, err, "failed")
+		err := doer.Do()
+		require.EqualError(t, err, "failed")
 
-	select {
-	case <-d1.calls:
-	default:
-		require.Fail(t, "doer1 not invoked")
-	}
+		select {
+		case <-d1.calls:
+		default:
+			require.Fail(t, "doer1 not invoked")
+		}
 
-	select {
-	case <-d3.calls:
-		require.Fail(t, "doer3 should not be invoked")
-	default:
-	}
+		select {
+		case <-d3.calls:
+			require.Fail(t, "doer3 should not be invoked")
+		default:
+		}
+	})
+
+	t.Run("success", func(t *testing.T) {
+		d1 := &testDoer{calls: make(chan bool, 1)}
+		d2 := &testDoer{calls: make(chan bool, 1)}
+		d3 := &testDoer{calls: make(chan bool, 1)}
+
+		doer := supervisor.MultiDoer{d1, d2, d3}
+
+		err := doer.Do()
+		require.NoError(t, err)
+
+		select {
+		case <-d1.calls:
+		default:
+			require.Fail(t, "doer1 not invoked")
+		}
+
+		select {
+		case <-d2.calls:
+		default:
+			require.Fail(t, "doer2 not invoked")
+		}
+
+		select {
+		case <-d3.calls:
+		default:
+			require.Fail(t, "doer3 not invoked")
+		}
+	})
 }
