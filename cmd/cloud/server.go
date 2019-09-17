140c74f7a402fafe8b13f6d9599f8faebacbd495
Gabe Jackson <gabe@coffeepowered.co>
2019-09-17 17:17:53 -0400

    Increase server connection timeouts (#80)
    This change increases the timeouts on the server connections to
    allow for long-running connections, such as Mattermost CLI commands,
    to finish in time.

--- cmd/cloud/server.go
+++ cmd/cloud/server.go
@@ -150,9 +150,9 @@ var serverCmd = &cobra.Command{
 		srv := &http.Server{
 			Addr:           listen,
 			Handler:        router,
-			ReadTimeout:    10 * time.Second,
-			WriteTimeout:   10 * time.Second,
-			IdleTimeout:    time.Second * 60,
+			ReadTimeout:    60 * time.Second,
+			WriteTimeout:   60 * time.Second,
+			IdleTimeout:    time.Second * 120,
 			MaxHeaderBytes: 1 << 20,
 			ErrorLog:       log.New(&logrusWriter{logger}, "", 0),
 		}
