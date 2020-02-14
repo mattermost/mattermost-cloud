5bf81417bfe052caf412b64c003297da95321115
Gabriel Linden Sagula <gsagula@gmail.com>
2020-02-14 00:11:19 -0800

    improved comments for session
    Signed-off-by: Gabriel Linden Sagula <gsagula@gmail.com>

--- internal/tools/aws/session.go
+++ internal/tools/aws/session.go
@@ -9,15 +9,14 @@ import (
 
 var singletonSessionOpts *SessionOpts
 
-// DefaultSessionConfig returns a singleton Session with default AWS config.
+// DefaultSessionConfig returns a singleton instance of SessionOpts with a default AWS config in it.
 func DefaultSessionConfig() *SessionOpts {
 	if singletonSessionOpts != nil {
 		singletonSessionOpts = &SessionOpts{
 			Opts: &session.Options{
 				Config: aws.Config{
 					Region: aws.String(DefaultAWSRegion),
-					// TODO(gsagula): we should supply a Retryer since it provides a more robust strategy. Ideally, we should delegate this
-					// kind of operations to a proxy or something that knows how to do that well.
+					// TODO(gsagula): we should use Retryer for a more robust retry strategy.
 					// https://github.com/aws/aws-sdk-go/blob/99cd35c8c7d369ba8c32c46ed306f6c88d24cfd7/aws/request/retryer.go#L20
 					MaxRetries: aws.Int(DefaultAWSClientRetries),
 				},
@@ -43,13 +42,10 @@ func (s *SessionOpts) CreateSession(logger log.FieldLogger) (*session.Session, e
 		return nil, err
 	}
 
-	setLoggerHandler(sess, logger)
-
-	return sess, nil
-}
-
-func setLoggerHandler(sess *session.Session, logger log.FieldLogger) {
+	// Log requests to AWS when the system is running in debug mode.
 	sess.Handlers.Send.PushFront(func(r *request.Request) {
 		logger.Debugf("%s: %s%s\n%s", r.HTTPRequest.Method, r.HTTPRequest.URL.Host, r.HTTPRequest.URL.RawPath, r.Params)
 	})
+
+	return sess, nil
 }
