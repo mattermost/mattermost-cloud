677f4ad27dc0e408aee95a0e7072abfc6efd0e16
Gabriel Linden Sagula <gsagula@gmail.com>
2020-03-25 12:24:33 -0700

    enhanced aws session HTTP handler
    Signed-off-by: Gabriel Linden Sagula <gsagula@gmail.com>

--- internal/tools/aws/session.go
+++ internal/tools/aws/session.go
@@ -1,6 +1,9 @@
 package aws
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -14,9 +17,26 @@ func NewAWSSessionWithLogger(config *aws.Config, logger log.FieldLogger) (*sessi
 		return nil, err
 	}
 
-	awsSession.Handlers.Send.PushFront(func(r *request.Request) {
-		logger.Debugf("%s: %s%s\n%s", r.HTTPRequest.Method, r.HTTPRequest.URL.Host, r.HTTPRequest.URL.RawPath, r.Params)
+	awsSession.Handlers.Complete.PushFront(func(r *request.Request) {
+		loggingMessage := fmt.Sprintf("%s.%s %s %s: %s%s  %q", r.ClientInfo.ServiceID, r.Operation.Name, r.HTTPRequest.Method,
+			r.HTTPResponse.Status, r.HTTPRequest.URL.Host, r.HTTPRequest.URL.RawPath, marshallParams(r.Params))
+
+		if r.HTTPResponse.StatusCode >= 400 {
+			logger.Error(loggingMessage)
+		}
+
+		if r.HTTPResponse.StatusCode < 400 {
+			logger.Debug(loggingMessage)
+		}
 	})
 
 	return awsSession, nil
+}
+
+func marshallParams(v interface{}) string {
+	paramBytes, err := json.Marshal(v)
+	if err != nil {
+		return err.Error()
+	}
+	return string(paramBytes)
 }
