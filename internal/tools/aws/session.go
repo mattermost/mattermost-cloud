b328b69e5353255c5ce2a5d56afbd644705a8b12
Gabriel Sagula <gsagula@gmail.com>
2020-03-27 02:13:48 -0700

    MM-23607: Enhanced AWS session HTTP handler (#175)
    * enhanced aws session HTTP handler
    
    Signed-off-by: Gabriel Linden Sagula <gsagula@gmail.com>

--- internal/tools/aws/session.go
+++ internal/tools/aws/session.go
@@ -1,9 +1,14 @@
 package aws
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,8 +19,31 @@ func NewAWSSessionWithLogger(config *aws.Config, logger log.FieldLogger) (*sessi
 		return nil, err
 	}
 
-	awsSession.Handlers.Send.PushFront(func(r *request.Request) {
-		logger.Debugf("%s: %s%s\n%s", r.HTTPRequest.Method, r.HTTPRequest.URL.Host, r.HTTPRequest.URL.RawPath, r.Params)
+	awsSession.Handlers.Complete.PushFront(func(r *request.Request) {
+		if r.HTTPResponse != nil && r.HTTPRequest != nil {
+			var buffer bytes.Buffer
+
+			buffer.WriteString(fmt.Sprintf("[aws] %s %s %s ", r.HTTPRequest.Method, r.HTTPResponse.Status, r.HTTPResponse.Request.URL.String()))
+
+			paramBytes, err := json.Marshal(r.Params)
+			if err != nil {
+				buffer.WriteString(err.Error())
+			} else {
+				buffer.Write(paramBytes)
+			}
+
+			logger = logger.WithFields(logrus.Fields{
+				"aws-service-id":     r.ClientInfo.ServiceID,
+				"aws-operation-name": r.Operation.Name,
+			})
+
+			if r.HTTPResponse.StatusCode >= 400 {
+				logger.Error(buffer.String())
+			}
+			if r.HTTPResponse.StatusCode < 400 {
+				logger.Debug(buffer.String())
+			}
+		}
 	})
 
 	return awsSession, nil
