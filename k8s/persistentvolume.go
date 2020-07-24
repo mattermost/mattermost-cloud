329cb3ad7d4678ca276d96538d20c5b4a9fb8938
Nick Misasi <nick.misasi@mattermost.com>
2020-07-24 09:09:59 -0400

    Adding support for PersistentVolume and PersistentVolumeClaim in k8s client

---
+++ k8s/persistentvolume.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+//
+
+package k8s
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (kc *KubeClient) createOrUpdatePersistentVolume(namespace string, volume *corev1.PersistentVolume) (metav1.Object, error) {
+	_, err := kc.Clientset.CoreV1().PersistentVolumes().Get(volume.GetName(), metav1.GetOptions{})
+	if err != nil && !k8sErrors.IsNotFound(err) {
+		return nil, err
+	}
+
+	if err != nil && k8sErrors.IsNotFound(err) {
+		return kc.Clientset.CoreV1().PersistentVolumes().Create(volume)
+	}
+
+	return kc.Clientset.CoreV1().PersistentVolumes().Update(volume)
+}
+
+func (kc *KubeClient) createOrUpdatePersistenVolumeClaim(namespace string, volumeClaim *corev1.PersistentVolumeClaim) (metav1.Object, error) {
+	_, err := kc.Clientset.CoreV1().PersistentVolumeClaims(namespace).Get(volumeClaim.GetName(), metav1.GetOptions{})
+	if err != nil && !k8sErrors.IsNotFound(err) {
+		return nil, err
+	}
+
+	if err != nil && k8sErrors.IsNotFound(err) {
+		return kc.Clientset.CoreV1().PersistentVolumeClaims(namespace).Create(volumeClaim)
+	}
+
+	return kc.Clientset.CoreV1().PersistentVolumeClaims(namespace).Update(volumeClaim)
+}
