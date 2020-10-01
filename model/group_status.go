2b94f0509f2222899535eea14d657948e76b56b9
Szymon Gibała <szymongib@gmail.com>
2020-10-01 10:01:19 +0200

    MM-27129 - Add group status endpoint (#312)
    * Add group status endpoint

---
+++ model/group_status.go
@@ -0,0 +1,29 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+//
+
+package model
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// GroupStatus represents the status of a group.
+type GroupStatus struct {
+	InstallationsTotal          int64
+	InstallationsUpdated        int64
+	InstallationsAwaitingUpdate int64
+}
+
+// GroupStatusFromReader decodes a json-encoded group status from the given io.Reader.
+func GroupStatusFromReader(reader io.Reader) (*GroupStatus, error) {
+	groupStatus := GroupStatus{}
+	decoder := json.NewDecoder(reader)
+	err := decoder.Decode(&groupStatus)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+
+	return &groupStatus, nil
+}
