31fec62b36bc6377f2c4a35919ea08d7d6cc904d
Ian Whitlock <ian.whitlock@mattermost.com>
2020-10-30 14:15:30 -0500

    Adds reports, runs, and other improvements to cloudburst

--- cmd/tools/cloudburst/cloudburst.go
+++ cmd/tools/cloudburst/cloudburst.go
@@ -7,6 +7,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 	"time"
 
@@ -20,10 +21,42 @@ import (
 
 var logger *log.Logger
 
+type completedReport struct {
+	installation *cloud.Installation
+	completedAt  time.Time
+	createdAt    time.Time
+}
+
+type errorReport struct {
+	installation *cloud.Installation
+	timestamp    time.Time
+	message      string
+}
+
+type ReportType string
+
+const (
+	ErrorReportType     ReportType = "errorReport"
+	CompletedReportType ReportType = "completedReport"
+)
+
+type Report interface {
+	Type() ReportType
+}
+
+func (e *errorReport) Type() ReportType {
+	return ErrorReportType
+}
+
+func (c *completedReport) Type() ReportType {
+	return CompletedReportType
+}
+
 type Blaster struct {
-	client *cloud.Client
-	testID string
-	group  *cloud.Group
+	client  *cloud.Client
+	testID  string
+	group   *cloud.Group
+	reports []Report
 }
 
 func NewBlaster(serverAddress string) *Blaster {
@@ -36,6 +69,10 @@ var blastCommand = &cobra.Command{
 	Short: "Run a load test against a provisioning server.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		serverAddress, _ := cmd.Flags().GetString("server")
+		batchSize, _ := cmd.Flags().GetInt("batch")
+		total, _ := cmd.Flags().GetInt("total")
+		runs, _ := cmd.Flags().GetInt("runs")
+
 		logger.SetLevel(log.DebugLevel)
 		logger.Infof("Server address %s", serverAddress)
 		logger.WithField("server", serverAddress)
@@ -47,15 +84,86 @@ var blastCommand = &cobra.Command{
 
 		logger.Infof("Waiting for Group to be created..")
 		blaster.waitForGroup()
-		logger.Infof("Requesting Installations be created..")
-		created := blaster.createInstallations(4, 4)
-		logger.Infof("Waiting for Installations to reconcile..")
-		blaster.waitForInstallations(created)
-		logger.Infof("Cleaning up Installations...")
-		blaster.cleanupInstallations(created)
-		logger.Infof("Removing the group...")
+
+		allReports := []Report{}
+		for i := 0; i < runs; i++ {
+			logger.Infof("Requesting Installations be created..")
+			created := blaster.createInstallations(total, batchSize)
+			logger.Infof("Waiting for Installations to reconcile..")
+			blaster.waitForInstallations(created)
+			logger.Infof("Cleaning up Installations...")
+			blaster.cleanupInstallations(created)
+			report := blaster.compileReports()
+			logger.Infof(
+				"\nCompleted test:\n\nErrors: %d\nSuccessful Installs: %d\nMinimum Time to Reconcile: %d seconds\nMedian Time to Reconcile: %d seconds\nMaximum Time to Reconcile: %d seconds\n\n",
+				report.errorCount,
+				report.successCount,
+				report.minDuration,
+				report.medianDuration,
+				report.maxDuration,
+			)
+			allReports = append(allReports, blaster.reports...)
+			blaster.reports = []Report{}
+		}
+		blaster.reports = allReports
+		report := blaster.compileReports()
+		logger.Infof(
+			"\nCompleted test:\n\nErrors: %d\nSuccessful Installs: %d\nMinimum Time to Reconcile: %d seconds\nMedian Time to Reconcile: %d seconds\nMaximum Time to Reconcile: %d seconds\n\n",
+			report.errorCount,
+			report.successCount,
+			report.minDuration,
+			report.medianDuration,
+			report.maxDuration,
+		)
+
 		return blaster.cleanupGroup()
 	},
+}
+
+type results struct {
+	errorCount     int64
+	successCount   int64
+	medianDuration int64
+	maxDuration    int64
+	minDuration    int64
+}
+
+type Durations []int64
+
+func (d Durations) Len() int {
+	return len(d)
+}
+
+func (d Durations) Less(i, j int) bool {
+	return d[i] < d[j]
+}
+
+func (d Durations) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+}
+
+func (b *Blaster) addReports(reports ...Report) {
+	b.reports = append(b.reports, reports...)
+}
+
+func (b *Blaster) compileReports() (output *results) {
+	output = new(results)
+	durations := []int64{}
+	for _, report := range b.reports {
+		switch r := report.(type) {
+		case *completedReport:
+			durations = append(durations, r.completedAt.Unix()-r.createdAt.Unix())
+			output.successCount++
+		case *errorReport:
+			output.errorCount++
+		}
+	}
+	sorted := Durations(durations)
+	sort.Sort(sorted)
+	output.maxDuration = sorted[len(sorted)-1]
+	output.minDuration = sorted[0]
+	output.medianDuration = sorted[len(sorted)/2]
+	return
 }
 
 func (b *Blaster) waitForInstallations(input map[string]*cloud.Installation) {
@@ -68,27 +176,47 @@ func (b *Blaster) waitForInstallations(input map[string]*cloud.Installation) {
 		for _, w := range waiting {
 			this, err := b.client.GetInstallation(w.ID, nil)
 			if err != nil {
-
+				logger.WithError(err).Warningf("failed to fetch Installation %s", w.ID)
+				continue
 			}
 			if this == nil {
-				logger.Errorf("Installation %s has gone missing")
+				b.addReports(&errorReport{
+					installation: this.Installation,
+					timestamp:    time.Now(),
+					message:      err.Error(),
+				})
+				logger.Errorf("Installation %s has gone missing", w.ID)
 				delete(waiting, this.ID)
 				continue
 			}
 			if strings.Contains(this.State, "failed") {
-				logger.Errorf("Installation %s failed to be created; not waiting for it")
+				logger.Errorf("Installation %s failed to be created; not waiting for it", this.ID)
+				b.addReports(&errorReport{
+					installation: this.Installation,
+					timestamp:    time.Now(),
+					message:      err.Error(),
+				})
 				delete(waiting, this.ID)
 				continue
 			}
 			if this.State != cloud.InstallationStateStable {
 				continue
 			}
+			r := &completedReport{
+				installation: this.Installation,
+				createdAt:    time.Unix(this.CreateAt/1000, 0),
+				completedAt:  time.Now(),
+			}
+			b.addReports(r)
+			log.Infof(
+				"Creation time for %s was: %d seconds",
+				r.installation.ID,
+				r.completedAt.Unix()-r.createdAt.Unix())
 			delete(waiting, this.ID)
 		}
 		if len(waiting) == 0 {
 			return
 		} else {
-			logger.Debugf("Waiting for %d installations to stabilize", len(waiting))
 			time.Sleep(5 * time.Second)
 		}
 	}
@@ -98,7 +226,7 @@ func (b *Blaster) waitForGroup() {
 	for {
 		g, err := b.client.GetGroup(b.group.ID)
 		if err != nil {
-			logger.WithError(err).Error("failed to get group %s", b.group.ID)
+			logger.WithError(err).Errorf("failed to get group %s", b.group.ID)
 			continue
 		}
 		if g == nil {
@@ -110,19 +238,22 @@ func (b *Blaster) waitForGroup() {
 func init() {
 	logger = log.New()
 	blastCommand.PersistentFlags().String("server", "http://localhost:8075", "Location of the Provisioning Server to load test")
+	blastCommand.PersistentFlags().Int("runs", 1, "Number of times to repeat the test")
+	blastCommand.PersistentFlags().Int("batch", 5, "Specify the number of Installations in each batch. Installations in a batch are install serially and batches are installed in parallel.")
+	blastCommand.PersistentFlags().Int("total", 20, "Number of Installations to provision")
 }
 
 func (b *Blaster) serialBatchInstall(count int) (installations []*cloud.Installation) {
 	for i := 0; i < count; i++ {
 		install, err := b.createInstallation()
 		if err != nil {
-			logger.WithError(err).Warn("failed to create Installation")
+			logger.WithError(err).Warn("failed to request Installation creation")
 			i--
+			continue
 		}
-		logger.Debugf("Requested creation successfully: %s", install.ID)
+		logger.Infof("Requested creation successfully: %s", install.ID)
 		installations = append(installations, install)
 	}
-	logger.Debugf("Successfully created %d installations", count)
 	return
 }
 
@@ -146,27 +277,29 @@ func (b *Blaster) cleanupInstallations(installations map[string]*cloud.Installat
 			fetched, err := b.client.GetInstallation(install.ID, nil)
 			if err != nil {
 				logger.WithError(err).Warnf("failed to look up Installation %s", install.ID)
+				continue
 			}
 			if fetched == nil {
 				logger.Warnf("Installation %s not found; will not retry deletion", install.ID)
+				b.addReports(&errorReport{
+					installation: install,
+					timestamp:    time.Now(),
+					message:      fmt.Sprintf("%s not found", install.ID),
+				})
 				delete(installations, install.ID)
 				continue
 			}
 			switch fetched.State {
 			case cloud.InstallationStateDeleted:
-				logger.Debugf("Successfully deleted Installation %s", fetched.ID)
+				logger.Infof("Successfully deleted Installation %s", fetched.ID)
 				delete(installations, fetched.ID)
-				continue
 			case cloud.InstallationStateDeletionRequested:
 				fallthrough
+			case cloud.InstallationStateDeletionFinalCleanup:
+				fallthrough
 			case cloud.InstallationStateDeletionInProgress:
-				continue
 			default:
-				err = b.client.DeleteInstallation(fetched.ID)
-				if err != nil {
-					logger.WithError(err).Warnf("failed to delete Installation %s", install.ID)
-					continue
-				}
+				b.client.DeleteInstallation(fetched.ID)
 			}
 		}
 	}
@@ -184,7 +317,6 @@ func (b *Blaster) createInstallations(total, batchSize int) map[string]*cloud.In
 	installationsChannel := make(chan []*cloud.Installation)
 	for i := 0; i < total; i += batchSize {
 		go func(out chan []*cloud.Installation, batchNumber int) {
-			logger.Debugf("Installing batch %d", batchNumber)
 			batch := b.serialBatchInstall(batchSize)
 			out <- batch
 		}(installationsChannel, i)
@@ -192,7 +324,6 @@ func (b *Blaster) createInstallations(total, batchSize int) map[string]*cloud.In
 
 	allInstallations := map[string]*cloud.Installation{}
 	for i := 0; i < total/batchSize; i++ {
-		logger.Debugf("Have %d need %d Installations. Receiving batch..", i, total)
 		batch := <-installationsChannel
 		for _, installation := range batch {
 			allInstallations[installation.ID] = installation
