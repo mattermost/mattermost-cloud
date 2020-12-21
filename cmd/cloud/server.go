ab780dadec62a01ce5bde0ec7598dc24d112c088
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2020-12-21 09:42:27 +0100

    webhooks: add environment to the webhooks payload (#386)

--- cmd/cloud/server.go
+++ cmd/cloud/server.go
@@ -210,6 +210,11 @@ var serverCmd = &cobra.Command{
 		}
 		awsClient := toolsAWS.NewAWSClientWithConfig(awsConfig, logger)
 
+		environment, err := awsClient.GetCloudEnvironmentName()
+		if err != nil {
+			return errors.Wrap(err, "getting the AWS Cloud environment")
+		}
+
 		err = checkRequirements(awsConfig, s3StateStore)
 		if err != nil {
 			return errors.Wrap(err, "failed health check")
@@ -264,6 +269,7 @@ var serverCmd = &cobra.Command{
 			Store:       sqlStore,
 			Supervisor:  supervisor,
 			Provisioner: kopsProvisioner,
+			Environment: environment,
 			Logger:      logger,
 		})
 
