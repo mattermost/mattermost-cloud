38552c3fba2968d756db7ca520d956858428b63d
Ian Whitlock <ian.whitlock@mattermost.com>
2021-05-10 10:04:08 -0500

    Give the mock packages unique names (#450)

--- internal/mocks/aws-sdk/doc.go
+++ internal/mocks/aws-sdk/doc.go
@@ -2,18 +2,18 @@
 // See LICENSE.txt for license information.
 //
 
-// Package mocks to create the mocks run go generate to regenerate this package.
-//go:generate ../../../bin/mockgen -package=mocks -destination ./ec2.go github.com/aws/aws-sdk-go/service/ec2/ec2iface EC2API
-//go:generate ../../../bin/mockgen -package=mocks -destination ./rds.go github.com/aws/aws-sdk-go/service/rds/rdsiface RDSAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./s3.go github.com/aws/aws-sdk-go/service/s3/s3iface S3API
-//go:generate ../../../bin/mockgen -package=mocks -destination ./acm.go github.com/aws/aws-sdk-go/service/acm/acmiface ACMAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./iam.go github.com/aws/aws-sdk-go/service/iam/iamiface IAMAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./route53.go github.com/aws/aws-sdk-go/service/route53/route53iface Route53API
-//go:generate ../../../bin/mockgen -package=mocks -destination ./kms.go github.com/aws/aws-sdk-go/service/kms/kmsiface KMSAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./secrets_manager.go github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface SecretsManagerAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./resource_tagging.go github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi/resourcegroupstaggingapiiface ResourceGroupsTaggingAPIAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./sts.go github.com/aws/aws-sdk-go/service/sts/stsiface STSAPI
-//go:generate ../../../bin/mockgen -package=mocks -destination ./dynamodb.go github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface DynamoDBAPI
+// Package mockawssdk to create the mocks run go generate to regenerate this package.
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./ec2.go github.com/aws/aws-sdk-go/service/ec2/ec2iface EC2API
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./rds.go github.com/aws/aws-sdk-go/service/rds/rdsiface RDSAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./s3.go github.com/aws/aws-sdk-go/service/s3/s3iface S3API
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./acm.go github.com/aws/aws-sdk-go/service/acm/acmiface ACMAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./iam.go github.com/aws/aws-sdk-go/service/iam/iamiface IAMAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./route53.go github.com/aws/aws-sdk-go/service/route53/route53iface Route53API
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./kms.go github.com/aws/aws-sdk-go/service/kms/kmsiface KMSAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./secrets_manager.go github.com/aws/aws-sdk-go/service/secretsmanager/secretsmanageriface SecretsManagerAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./resource_tagging.go github.com/aws/aws-sdk-go/service/resourcegroupstaggingapi/resourcegroupstaggingapiiface ResourceGroupsTaggingAPIAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./sts.go github.com/aws/aws-sdk-go/service/sts/stsiface STSAPI
+//go:generate ../../../bin/mockgen -package=mockawssdk -destination ./dynamodb.go github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface DynamoDBAPI
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt ec2.go > _ec2.go && mv _ec2.go ec2.go"
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt rds.go > _rds.go && mv _rds.go rds.go"
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt s3.go > _s3.go && mv _s3.go s3.go"
@@ -25,4 +25,4 @@
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt resource_tagging.go > _resource_tagging.go && mv _resource_tagging.go resource_tagging.go"
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt sts.go > _sts.go && mv _sts.go sts.go"
 //go:generate /usr/bin/env bash -c "cat ../../../hack/boilerplate/boilerplate.generatego.txt dynamodb.go > _dynamodb.go && mv _dynamodb.go dynamodb.go"
-package mocks //nolint
+package mockawssdk //nolint
