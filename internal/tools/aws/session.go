36652d6f7d2d1fc4d3a25430b5b6faa3b5d9cf08
Szymon Gibała <szymongib@gmail.com>
2022-01-13 09:31:14 +0100

    Sanitize AWS requests

--- internal/tools/aws/session.go
+++ internal/tools/aws/session.go
@@ -8,6 +8,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -15,6 +16,17 @@ import (
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
+
+const redactedParam = "*****"
+
+// Specifies AWS request parameters that should be sanitized from logs.
+var sanitizeLogParams = map[string]bool{
+	"secretstring":       true,
+	"secretbinary":       true,
+	"masterpassword":     true,
+	"masteruserpassword": true,
+	"masterusername":     true,
+}
 
 // NewAWSSessionWithLogger initializes an AWS session instance with logging handler for debuging only.
 func NewAWSSessionWithLogger(config *aws.Config, logger log.FieldLogger) (*session.Session, error) {
@@ -29,11 +41,9 @@ func NewAWSSessionWithLogger(config *aws.Config, logger log.FieldLogger) (*sessi
 
 			buffer.WriteString(fmt.Sprintf("[aws] %s %s (%s)", r.HTTPRequest.Method, r.HTTPRequest.URL.String(), r.HTTPResponse.Status))
 
-			paramBytes, err := json.Marshal(r.Params)
-			if err != nil {
-				buffer.WriteString(err.Error())
-			} else {
-				logger = logger.WithField("params", string(paramBytes))
+			if r.ParamsFilled() {
+				params := sanitizeParams(r.Params, logger)
+				logger = logger.WithField("params", params)
 			}
 
 			logger = logger.WithFields(logrus.Fields{
@@ -46,4 +56,36 @@ func NewAWSSessionWithLogger(config *aws.Config, logger log.FieldLogger) (*sessi
 	})
 
 	return awsSession, nil
+}
+
+// This is far from an ideal way to handle the sanitization
+// but we can retain some AWS logs and debuggability without investing
+// to much time into sophisticated sanitization/logging mechanism.
+func sanitizeParams(reqParams interface{}, logger logrus.FieldLogger) string {
+	paramsJSON, err := json.Marshal(reqParams)
+	if err != nil {
+		logger.WithError(err).Warn("Failed to marshal AWS request parameters")
+		return "<ALL_PARAMETERS_REDACTED>"
+	}
+
+	var paramMap map[string]interface{}
+	err = json.Unmarshal(paramsJSON, &paramMap)
+	if err != nil {
+		logger.WithError(err).Warn("Failed to unmarshal AWS request parameters to map[string]interface{}")
+		return "<ALL_PARAMETERS_REDACTED>"
+	}
+
+	for k := range paramMap {
+		if sanitizeLogParams[strings.ToLower(k)] {
+			paramMap[k] = redactedParam
+		}
+	}
+
+	redactedJSON, err := json.Marshal(paramMap)
+	if err != nil {
+		logger.WithError(err).Warn("Failed to marshal sanitized AWS request params")
+		return "<ALL_PARAMETERS_REDACTED>"
+	}
+
+	return string(redactedJSON)
 }
