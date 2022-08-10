f00782cbccc3309f291e6b06ce7fc6dd34a045a7
Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>
2022-08-10 17:03:44 +0300

    CLD-3928 Revert Thanos back on 0.26.0 version to fix introduced decoupling bugs

--- model/cluster_utility.go
+++ model/cluster_utility.go
@@ -64,7 +64,7 @@ var DefaultUtilityVersions map[string]*HelmUtilityVersion = map[string]*HelmUtil
 	// PrometheusOperatorCanonicalName defines the default version and values path for the Helm chart
 	PrometheusOperatorCanonicalName: {Chart: "35.3.1", ValuesPath: ""},
 	// ThanosCanonicalName defines the default version and values path for the Helm chart
-	ThanosCanonicalName: {Chart: "11.1.3", ValuesPath: ""},
+	ThanosCanonicalName: {Chart: "10.5.4", ValuesPath: ""},
 	// NginxCanonicalName defines the default version and values path for the Helm chart
 	NginxCanonicalName: {Chart: "4.0.18", ValuesPath: ""},
 	// NginxInternalCanonicalName defines the default version and values path for the Helm chart
