afc5b8f1c5c2b20e595f0d8f342c8c791f94aeb7
Felipe Martin <me@fmartingr.com>
2022-10-25 20:39:08 +0200

    Allow CLI to retrieve groups installation count

--- model/group_request.go
+++ model/group_request.go
@@ -156,13 +156,16 @@ func NewPatchGroupRequestFromReader(reader io.Reader) (*PatchGroupRequest, error
 // GetGroupsRequest describes the parameters to request a list of groups.
 type GetGroupsRequest struct {
 	Paging
+	WithInstallationCount bool
 }
 
 // ApplyToURL modifies the given url to include query string parameters for the request.
 func (request *GetGroupsRequest) ApplyToURL(u *url.URL) {
 	q := u.Query()
+	if request.WithInstallationCount {
+		q.Add("with_installation_count", "true")
+	}
 	request.Paging.AddToQuery(q)
-
 	u.RawQuery = q.Encode()
 }
 
