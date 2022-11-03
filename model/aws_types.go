97eccd54a53e529449ecf11eae6477f6f07f2267
Felipe Martin <me@fmartingr.com>
2022-11-03 12:38:26 +0100

    Added license to added files

--- model/aws_types.go
+++ model/aws_types.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+// 
+
 package model
 
 // Certificate represents an AWS ACM certificate
