3fae1b412bfd9f092c63db0d830cbbf8f03f60a2
Spiros Economakis <spiros.oikonomakis@gmail.com>
2022-12-14 16:43:15 +0200

    Migrate from CircleCI to Github Actions
    We gradually moving away from CirleCI to Github Actions as we are consolidating
    the CIs we use. In parallel we are introducing `trivy` in the PR level
    and we block the PR about the number of critical vulnerabilities.
    
    Ticket: https://mattermost.atlassian.net/browse/CLD-4697

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -3,18 +3,9 @@
 # Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 # See LICENSE.txt for license information.
 
-set -e
-set -u
+set -euox
 
-if [[ -z "${CIRCLE_TAG:-}" ]]; then
-  echo "Pushing lastest for $CIRCLE_BRANCH..."
-  TAG=latest
-else
-  echo "Pushing release $CIRCLE_TAG..."
-  TAG="$CIRCLE_TAG"
-fi
-
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+echo $DOCKER_TOKEN | docker login --username $DOCKER_USERNAME --password-stdin
 
 docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:$TAG
 docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:$TAG
