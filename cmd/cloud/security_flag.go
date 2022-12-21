a989989e3679e78add2e9ffca9706b6f2b5b4257
Mir Shahriar Sabuj <mir.shahriar@mattermost.com>
2022-12-21 19:12:39 +0600

    added files

---
+++ cmd/cloud/security_flag.go
@@ -0,0 +1,86 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func setSecurityFlags(command *cobra.Command) {
+	command.PersistentFlags().String("server", defaultLocalServerAPI, "The provisioning server whose API will be queried.")
+}
+
+type securityFlags struct {
+	serverAddress string
+}
+
+func (flags *securityFlags) addFlags(command *cobra.Command) {
+	flags.serverAddress, _ = command.Flags().GetString("server")
+}
+
+func setSecurityClusterFlags(command *cobra.Command) {
+	command.PersistentFlags().String("cluster", "", "The id of the cluster.")
+	_ = command.MarkPersistentFlagRequired("cluster")
+}
+
+type securityClusterFlags struct {
+	securityFlags
+	clusterID string
+}
+
+func (flags *securityClusterFlags) addFlags(command *cobra.Command) {
+	flags.securityFlags.addFlags(command)
+	flags.clusterID, _ = command.Flags().GetString("cluster")
+}
+
+func setSecurityInstallationFlags(command *cobra.Command) {
+	command.PersistentFlags().String("installation", "", "The id of the installation.")
+	_ = command.MarkPersistentFlagRequired("installation")
+}
+
+type securityInstallationFlags struct {
+	securityFlags
+	installationID string
+}
+
+func (flags *securityInstallationFlags) addFlags(command *cobra.Command) {
+	flags.installationID, _ = command.Flags().GetString("installation")
+}
+
+func setSecurityClusterInstallationFlags(command *cobra.Command) {
+	command.PersistentFlags().String("cluster-installation", "", "The id of the cluster installation.")
+	_ = command.MarkPersistentFlagRequired("cluster-installation")
+}
+
+type securityClusterInstallationFlags struct {
+	securityFlags
+	clusterInstallationID string
+}
+
+func (flags *securityClusterInstallationFlags) addFlags(command *cobra.Command) {
+	flags.clusterInstallationID, _ = command.Flags().GetString("cluster-installation")
+}
+
+func setSecurityGroupFlags(command *cobra.Command) {
+	command.PersistentFlags().String("group", "", "The id of the group.")
+	_ = command.MarkPersistentFlagRequired("group")
+}
+
+type securityGroupFlags struct {
+	securityFlags
+	groupID string
+}
+
+func (flags *securityGroupFlags) addFlags(command *cobra.Command) {
+	flags.groupID, _ = command.Flags().GetString("group")
+}
+
+func setSecurityBackupFlags(command *cobra.Command) {
+	command.PersistentFlags().String("backup", "", "The id of the backup.")
+	_ = command.MarkPersistentFlagRequired("backup")
+}
+
+type securityBackupFlags struct {
+	securityFlags
+	backupID string
+}
+
+func (flags *securityBackupFlags) addFlags(command *cobra.Command) {
+	flags.backupID, _ = command.Flags().GetString("backup")
+}
---
+++ cmd/cloud/webhook_flag.go
@@ -0,0 +1,61 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func setWebhookFlags(command *cobra.Command) {
+	command.PersistentFlags().String("server", defaultLocalServerAPI, "The provisioning server whose API will be queried.")
+}
+
+type webhookFlags struct {
+	serverAddress string
+}
+
+func (flags *webhookFlags) addFlags(command *cobra.Command) {
+	flags.serverAddress, _ = command.Flags().GetString("server")
+}
+
+type webhookCreateFlag struct {
+	webhookFlags
+	ownerID string
+	url     string
+}
+
+func (flags *webhookCreateFlag) addFlags(command *cobra.Command) {
+	command.Flags().StringVar(&flags.ownerID, "owner", "", "An opaque identifier describing the owner of the webhook.")
+	command.Flags().StringVar(&flags.url, "url", "", "The callback URL of the webhook.")
+	_ = command.MarkFlagRequired("owner")
+	_ = command.MarkFlagRequired("url")
+}
+
+type webhookGetFlag struct {
+	webhookFlags
+	webhookID string
+}
+
+func (flags *webhookGetFlag) addFlags(command *cobra.Command) {
+	command.Flags().StringVar(&flags.webhookID, "webhook", "", "The id of the webhook to be fetched.")
+	_ = command.MarkFlagRequired("webhook")
+}
+
+type webhookListFlag struct {
+	webhookFlags
+	pagingFlags
+	owner         string
+	outputToTable bool
+}
+
+func (flags *webhookListFlag) addFlags(command *cobra.Command) {
+	flags.pagingFlags.addFlags(command)
+	command.Flags().StringVar(&flags.owner, "owner", "", "The owner by which to filter webhooks.")
+	command.Flags().BoolVar(&flags.outputToTable, "table", false, "Whether to display the returned webhook list in a table or not")
+}
+
+type webhookDeleteFlag struct {
+	webhookFlags
+	webhookID string
+}
+
+func (flags *webhookDeleteFlag) addFlags(command *cobra.Command) {
+	command.Flags().StringVar(&flags.webhookID, "webhook", "", "The id of the webhook to be deleted.")
+	_ = command.MarkFlagRequired("webhook")
+}
