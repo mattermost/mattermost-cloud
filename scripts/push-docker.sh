dca64acb04a386f95e68f2d55a619db6b09a2ee9
Gabe Jackson <gabe@coffeepowered.co>
2023-01-13 13:03:43 -0500

    Tag docker images with latest tag (#833)

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -7,12 +7,25 @@ set -euox
 
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 
-if [ "$TAG" = "master" ] || [ "$TAG" = "main" ]; then
-    TAG=latest
+if [ "$COMMIT_SHA" = "" ]; then
+    echo "COMMIT_SHA was not provided"
+    exit 1
 fi
 
-docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:$TAG
-docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:$TAG
+echo "Tagging images with SHA $COMMIT_SHA"
 
-docker push mattermost/mattermost-cloud:$TAG
-docker push mattermost/mattermost-cloud-e2e:$TAG
+docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:$COMMIT_SHA
+docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:$COMMIT_SHA
+
+docker push mattermost/mattermost-cloud:$COMMIT_SHA
+docker push mattermost/mattermost-cloud-e2e:$COMMIT_SHA
+
+if [ "$REF_NAME" = "master" ] || [ "$REF_NAME" = "main" ]; then
+    echo "Tagging images with 'latest' tag"
+
+    docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:latest
+    docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:latest
+
+    docker push mattermost/mattermost-cloud:latest
+    docker push mattermost/mattermost-cloud-e2e:latest
+fi
