47863d14d9ac243dccad6885099631aae2608bbf
Felipe Martin <me@fmartingr.com>
2023-01-20 10:08:27 +0100

    chore: add more logging info to kops invoke error

--- internal/tools/kops/export.go
+++ internal/tools/kops/export.go
@@ -4,19 +4,24 @@
 
 package kops
 
-import "github.com/pkg/errors"
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
 
 // ExportKubecfg invokes kops export kubecfg for the named cluster in the context of the created Cmd.
 func (c *Cmd) ExportKubecfg(name string) error {
-	_, _, err := c.run(
+	_, stderr, err := c.run(
 		"export",
 		"kubecfg",
 		arg("name", name),
 		arg("state", "s3://", c.s3StateStore),
 		arg("admin", "87600h"),
 	)
+
 	if err != nil {
-		return errors.Wrap(err, "failed to invoke kops export kubecfg")
+		return errors.Wrap(err, fmt.Sprintf("failed to invoke kops export kubecfg: %s", string(stderr)))
 	}
 
 	return nil
