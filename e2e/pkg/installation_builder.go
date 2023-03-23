686dc7740ef3f5a8b07f16591cdf483edfcc189c
Mir Shahriar Sabuj <mir.shahriar@mattermost.com>
2023-03-23 11:44:55 +0600

    Use 7.9.1 for mattermost-server in E2E test (#894)

--- e2e/pkg/installation_builder.go
+++ e2e/pkg/installation_builder.go
@@ -2,7 +2,8 @@
 // See LICENSE.txt for license information.
 //
 
-//+build e2e
+//go:build e2e
+// +build e2e
 
 package pkg
 
@@ -17,7 +18,7 @@ type InstallationBuilder struct {
 func NewInstallationBuilderWithDefaults() *InstallationBuilder {
 	return &InstallationBuilder{request: &model.CreateInstallationRequest{
 		OwnerID:   "e2e-test",
-		Version:   "stable",
+		Version:   "7.9.1",
 		Image:     "mattermost/mattermost-enterprise-edition",
 		Size:      "1000users",
 		Affinity:  "multitenant",
