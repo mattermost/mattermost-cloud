8fe4a62c4687db6d8a66d63b4ac0565dd8fe0563
Felipe Martin <me@fmartingr.com>
2023-05-22 14:26:21 +0200

    restore sqlite migrations

--- internal/store/migrations.go
+++ internal/store/migrations.go
@@ -84,6 +84,58 @@ var migrations = []migration{
 			if err != nil {
 				return err
 			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE Cluster RENAME TO ClusterTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				CREATE TABLE Cluster (
+					ID CHAR(26) PRIMARY KEY,
+					Provider VARCHAR(32) NOT NULL,
+					Provisioner VARCHAR(32) NOT NULL,
+					ProviderMetadata BYTEA NULL,
+					ProvisionerMetadata BYTEA NULL,
+					Size VARCHAR(32) NOT NULL,
+					State VARCHAR(32) NOT NULL,
+					AllowInstallations BOOLEAN NOT NULL,
+					CreateAt BIGINT NOT NULL,
+					DeleteAt BIGINT NOT NULL,
+					LockAcquiredBy CHAR(26) NULL,
+					LockAcquiredAt BIGINT NOT NULL
+				);
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				INSERT INTO Cluster
+				SELECT
+					ID,
+					Provider,
+					Provisioner,
+					ProviderMetadata,
+					ProvisionerMetadata,
+					"SizeAlef500",
+					"stable",
+					AllowInstallations,
+					CreateAt,
+					DeleteAt,
+					LockAcquiredBy,
+					LockAcquiredAt
+				FROM
+					ClusterTemp;
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE ClusterTemp;`)
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
@@ -159,7 +211,8 @@ var migrations = []migration{
 		return nil
 	}},
 	{semver.MustParse("0.4.0"), semver.MustParse("0.5.0"), func(e execer) error {
-		// Switch various columns to TEXT for Postgres.
+		// Switch various columns to TEXT for Postgres. This isn't actually necessary for
+		// SQLite, and it's harder to change types there anyway.
 		if e.DriverName() == driverPostgres {
 			_, err := e.Exec(`ALTER TABLE System ALTER COLUMN Key TYPE TEXT;`)
 			if err != nil {
@@ -255,9 +308,61 @@ var migrations = []migration{
 		return nil
 	}},
 	{semver.MustParse("0.5.0"), semver.MustParse("0.6.0"), func(e execer) error {
-		// Add the new Size column.
+		// Add the new Size column. SQLite will need to re-create manually.
 		if e.DriverName() == driverPostgres {
 			_, err := e.Exec(`ALTER TABLE Installation ADD COLUMN Size TEXT DEFAULT '100users';`)
+			if err != nil {
+				return err
+			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE Installation RENAME TO InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					CREATE TABLE Installation (
+						ID TEXT PRIMARY KEY,
+						OwnerID TEXT NOT NULL,
+						Version TEXT NOT NULL,
+						DNS TEXT NOT NULL,
+						Size TEXT NOT NULL,
+						Affinity TEXT NOT NULL,
+						GroupID TEXT NULL,
+						State TEXT NOT NULL,
+						CreateAt BIGINT NOT NULL,
+						DeleteAt BIGINT NOT NULL,
+						LockAcquiredBy TEXT NULL,
+						LockAcquiredAt BIGINT NOT NULL
+					);
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					INSERT INTO Installation
+					SELECT
+						ID,
+						OwnerID,
+						Version,
+						DNS,
+						"100users",
+						Affinity,
+						GroupID,
+						State,
+						CreateAt,
+						DeleteAt,
+						LockAcquiredBy,
+						LockAcquiredAt
+					FROM
+					InstallationTemp;
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE InstallationTemp;`)
 			if err != nil {
 				return err
 			}
@@ -331,6 +436,64 @@ var migrations = []migration{
 			if err != nil {
 				return err
 			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE Installation RENAME TO InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					CREATE TABLE Installation (
+						ID TEXT PRIMARY KEY,
+						OwnerID TEXT NOT NULL,
+						Version TEXT NOT NULL,
+						DNS TEXT NOT NULL,
+						Database TEXT NOT NULL,
+						Filestore TEXT NOT NULL,
+						License TEXT NULL,
+						Size TEXT NOT NULL,
+						Affinity TEXT NOT NULL,
+						GroupID TEXT NULL,
+						State TEXT NOT NULL,
+						CreateAt BIGINT NOT NULL,
+						DeleteAt BIGINT NOT NULL,
+						LockAcquiredBy TEXT NULL,
+						LockAcquiredAt BIGINT NOT NULL
+					);
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					INSERT INTO Installation
+					SELECT
+						ID,
+						OwnerID,
+						Version,
+						DNS,
+						"mysql-operator",
+						"minio-operator",
+						License,
+						Size,
+						Affinity,
+						GroupID,
+						State,
+						CreateAt,
+						DeleteAt,
+						LockAcquiredBy,
+						LockAcquiredAt
+					FROM
+					InstallationTemp;
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE InstallationTemp;`)
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
@@ -341,12 +504,18 @@ var migrations = []migration{
 			if err != nil {
 				return err
 			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`UPDATE Cluster SET AllowInstallations = '1';`)
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
 	}},
 	{semver.MustParse("0.10.0"), semver.MustParse("0.11.0"), func(e execer) error {
 		// Add Cluster Version column.
+		// Also convert SQLite char/varchar columns to type TEXT to match PG.
 		if e.DriverName() == driverPostgres {
 			_, err := e.Exec(`ALTER TABLE Cluster ADD COLUMN Version TEXT NULL;`)
 			if err != nil {
@@ -359,6 +528,60 @@ var migrations = []migration{
 			}
 
 			_, err = e.Exec(`ALTER TABLE Cluster ALTER COLUMN Version SET NOT NULL; `)
+			if err != nil {
+				return err
+			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE Cluster RENAME TO ClusterTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				CREATE TABLE Cluster (
+					ID TEXT PRIMARY KEY,
+					Provider TEXT NOT NULL,
+					Provisioner TEXT NOT NULL,
+					ProviderMetadata BYTEA NULL,
+					ProvisionerMetadata BYTEA NULL,
+					Version TEXT NOT NULL,
+					Size TEXT NOT NULL,
+					State TEXT NOT NULL,
+					AllowInstallations BOOLEAN NOT NULL,
+					CreateAt BIGINT NOT NULL,
+					DeleteAt BIGINT NOT NULL,
+					LockAcquiredBy TEXT NULL,
+					LockAcquiredAt BIGINT NOT NULL
+				);
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				INSERT INTO Cluster
+				SELECT
+					ID,
+					Provider,
+					Provisioner,
+					ProviderMetadata,
+					ProvisionerMetadata,
+					"0.0.0",
+					Size,
+					State,
+					AllowInstallations,
+					CreateAt,
+					DeleteAt,
+					LockAcquiredBy,
+					LockAcquiredAt
+				FROM
+					ClusterTemp;
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE ClusterTemp;`)
 			if err != nil {
 				return err
 			}
@@ -382,7 +605,8 @@ var migrations = []migration{
 		// exist, otherwise, keep existing values
 		_, err := e.Exec(`
 				UPDATE Cluster
-				SET UtilityMetadata = '{}' WHERE UtilityMetadata is NULL;`)
+				SET UtilityMetadata = '{}' WHERE UtilityMetadata is NULL;
+		 `)
 		return err
 	}},
 	{semver.MustParse("0.12.1"), semver.MustParse("0.13.0"), func(e execer) error {
@@ -409,6 +633,7 @@ var migrations = []migration{
 		// Add Group Sequence column for installations and groups.
 		// Add Group MaxRolling column.
 		// Add Group lock columns.
+		// tl;dr we should get rid of SQLite
 
 		_, err := e.Exec(`ALTER TABLE Installation ADD COLUMN GroupSequence BIGINT NULL;`)
 		if err != nil {
@@ -460,7 +685,58 @@ var migrations = []migration{
 				return err
 			}
 
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE "Group" RENAME TO "GroupTemp";`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				CREATE TABLE "Group" (
+					ID TEXT PRIMARY KEY,
+					Name TEXT,
+					Description TEXT,
+					Version TEXT,
+					MattermostEnvRaw BYTEA NULL,
+					MaxRolling BIGINT NOT NULL,
+					Sequence BIGINT NOT NULL,
+					CreateAt BIGINT NOT NULL,
+					DeleteAt BIGINT NOT NULL,
+					LockAcquiredBy TEXT NULL,
+					LockAcquiredAt BIGINT NOT NULL
+				);
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					INSERT INTO "Group"
+					SELECT
+						ID,
+						Name,
+						Description,
+						Version,
+						MattermostEnvRaw,
+						1,
+						0,
+						CreateAt,
+						DeleteAt,
+						NULL,
+						0
+					FROM
+						"GroupTemp";
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE "GroupTemp";`)
+			if err != nil {
+				return err
+			}
 		}
+
 		return nil
 	}},
 	{semver.MustParse("0.15.0"), semver.MustParse("0.16.0"), func(e execer) error {
@@ -505,6 +781,122 @@ var migrations = []migration{
 			}
 
 			_, err = e.Exec(`ALTER TABLE "Group" ALTER COLUMN Image SET NOT NULL;`)
+			if err != nil {
+				return err
+			}
+		} else if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`ALTER TABLE Installation RENAME TO InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					CREATE TABLE Installation (
+						ID TEXT PRIMARY KEY,
+						OwnerID TEXT NOT NULL,
+						Version TEXT NOT NULL,
+						Image TEXT NOT NULL,
+						DNS TEXT NOT NULL,
+						Database TEXT NOT NULL,
+						Filestore TEXT NOT NULL,
+						License TEXT NULL,
+						Size TEXT NOT NULL,
+						MattermostEnvRaw BYTEA NULL,
+						Affinity TEXT NOT NULL,
+						GroupSequence BIGINT NULL,
+						GroupID TEXT NULL,
+						State TEXT NOT NULL,
+						CreateAt BIGINT NOT NULL,
+						DeleteAt BIGINT NOT NULL,
+						LockAcquiredBy TEXT NULL,
+						LockAcquiredAt BIGINT NOT NULL
+					);
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					INSERT INTO Installation
+					SELECT
+						ID,
+						OwnerID,
+						Version,
+						"mattermost/mattermost-enterprise-edition",
+						DNS,
+						Database,
+						Filestore,
+						License,
+						Size,
+						MattermostEnvRaw,
+						Affinity,
+						GroupSequence,
+						GroupID,
+						State,
+						CreateAt,
+						DeleteAt,
+						LockAcquiredBy,
+						LockAcquiredAt
+					FROM
+					InstallationTemp;
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`ALTER TABLE "Group" RENAME TO "GroupTemp";`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				CREATE TABLE "Group" (
+					ID TEXT PRIMARY KEY,
+					Name TEXT,
+					Description TEXT,
+					Version TEXT,
+					Image TEXT,
+					MattermostEnvRaw BYTEA NULL,
+					MaxRolling BIGINT NOT NULL,
+					Sequence BIGINT NOT NULL,
+					CreateAt BIGINT NOT NULL,
+					DeleteAt BIGINT NOT NULL,
+					LockAcquiredBy TEXT NULL,
+					LockAcquiredAt BIGINT NOT NULL
+				);
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				INSERT INTO "Group"
+					SELECT
+						ID,
+						Name,
+						Description,
+						Version,
+						"mattermost/mattermost-enterprise-edition",
+						MattermostEnvRaw,
+						MaxRolling,
+						Sequence,
+						CreateAt,
+						DeleteAt,
+						LockAcquiredBy,
+						LockAcquiredAt
+					FROM
+						"GroupTemp";
+					`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE GroupTemp;`)
 			if err != nil {
 				return err
 			}
@@ -942,7 +1334,12 @@ var migrations = []migration{
 		if err != nil {
 			return err
 		}
-
+		if e.DriverName() == driverSqlite {
+			_, err := e.Exec(`UPDATE ClusterInstallation SET IsActive = '1';`)
+			if err != nil {
+				return err
+			}
+		}
 		return nil
 	}},
 	{semver.MustParse("0.29.0"), semver.MustParse("0.30.0"), func(e execer) error {
@@ -1375,6 +1772,11 @@ var migrations = []migration{
 			if err != nil {
 				return errors.Wrap(err, "failed to set name based on DNS")
 			}
+		} else if e.DriverName() == driverSqlite {
+			_, err = e.Exec("UPDATE Installation SET name = (SUBSTR(DNS, 0, INSTR(DNS, '.')));")
+			if err != nil {
+				return err
+			}
 		}
 
 		rows, err := e.Query("SELECT id, dns, createAt, deleteAt FROM INSTALLATION;")
@@ -1436,6 +1838,79 @@ var migrations = []migration{
 			_, err = e.Exec("ALTER TABLE Installation ALTER COLUMN name SET NOT NULL;")
 			if err != nil {
 				return errors.Wrap(err, "failed to remove not null name constraint")
+			}
+		} else if e.DriverName() == driverSqlite {
+			// We DROP DNS here and add NOT NULL for Name
+			_, err = e.Exec(`ALTER TABLE Installation RENAME TO InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					CREATE TABLE Installation (
+						ID TEXT PRIMARY KEY,
+						OwnerID TEXT NOT NULL,
+						Name TEXT NOT NULL,
+						Version TEXT NOT NULL,
+						Image TEXT NOT NULL,
+						Database TEXT NOT NULL,
+						Filestore TEXT NOT NULL,
+						License TEXT NULL,
+						Size TEXT NOT NULL,
+						MattermostEnvRaw BYTEA NULL,
+						PriorityEnvRaw BYTEA NULL,
+						Affinity TEXT NOT NULL,
+						GroupSequence BIGINT NULL,
+						GroupID TEXT NULL,
+						State TEXT NOT NULL,
+						APISecurityLock NOT NULL DEFAULT FALSE,
+						SingleTenantDatabaseConfigRaw BYTEA NULL,
+						CRVersion TEXT NOT NULL DEFAULT 'mattermost.com/v1alpha1',
+						CreateAt BIGINT NOT NULL,
+						DeleteAt BIGINT NOT NULL,
+						LockAcquiredBy TEXT NULL,
+						LockAcquiredAt BIGINT NOT NULL
+					);
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+					INSERT INTO Installation
+					SELECT
+						ID,
+						OwnerID,
+						Name,
+						Version,
+						Image,
+						Database,
+						Filestore,
+						License,
+						Size,
+						MattermostEnvRaw,
+						PriorityEnvRaw,
+						Affinity,
+						GroupSequence,
+						GroupID,
+						State,
+						APISecurityLock,
+						SingleTenantDatabaseConfigRaw,
+						CRVersion,
+						CreateAt,
+						DeleteAt,
+						LockAcquiredBy,
+						LockAcquiredAt
+					FROM
+					InstallationTemp;
+				`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE InstallationTemp;`)
+			if err != nil {
+				return err
 			}
 		}
 
@@ -1560,12 +2035,106 @@ var migrations = []migration{
 			if err != nil {
 				return errors.Wrap(err, "failed to remove not null expiry constraint")
 			}
+		} else if e.DriverName() == driverSqlite {
+			// We DROP DNS here and add NOT NULL for Name
+			_, err := e.Exec(`ALTER TABLE Installation RENAME TO InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				CREATE TABLE Installation (
+					ID TEXT PRIMARY KEY,
+					OwnerID TEXT NOT NULL,
+					Name TEXT NOT NULL,
+					Version TEXT NOT NULL,
+					Image TEXT NOT NULL,
+					Database TEXT NOT NULL,
+					Filestore TEXT NOT NULL,
+					License TEXT NULL,
+					Size TEXT NOT NULL,
+					MattermostEnvRaw BYTEA NULL,
+					PriorityEnvRaw BYTEA NULL,
+					Affinity TEXT NOT NULL,
+					GroupSequence BIGINT NULL,
+					GroupID TEXT NULL,
+					State TEXT NOT NULL,
+					APISecurityLock NOT NULL DEFAULT FALSE,
+					SingleTenantDatabaseConfigRaw BYTEA NULL,
+					ExternalDatabaseConfigRaw BYTEA NULL,
+					CRVersion TEXT NOT NULL DEFAULT 'mattermost.com/v1alpha1',
+					CreateAt BIGINT NOT NULL,
+					DeleteAt BIGINT NOT NULL,
+					DeletionPendingExpiry BIGINT NOT NULL,
+					LockAcquiredBy TEXT NULL,
+					LockAcquiredAt BIGINT NOT NULL
+				);
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`
+				INSERT INTO Installation
+				SELECT
+					ID,
+					OwnerID,
+					Name,
+					Version,
+					Image,
+					Database,
+					Filestore,
+					License,
+					Size,
+					MattermostEnvRaw,
+					PriorityEnvRaw,
+					Affinity,
+					GroupSequence,
+					GroupID,
+					State,
+					APISecurityLock,
+					SingleTenantDatabaseConfigRaw,
+					ExternalDatabaseConfigRaw,
+					CRVersion,
+					CreateAt,
+					DeleteAt,
+					'0',
+					LockAcquiredBy,
+					LockAcquiredAt
+				FROM
+					InstallationTemp;
+			`)
+			if err != nil {
+				return err
+			}
+
+			_, err = e.Exec(`DROP TABLE InstallationTemp;`)
+			if err != nil {
+				return err
+			}
+
+			// Recreate indexes.
+			_, err = e.Exec("CREATE UNIQUE INDEX Installation_Name_DeleteAt ON Installation (Name, DeleteAt);")
+			if err != nil {
+				return err
+			}
+			_, err = e.Exec(`CREATE INDEX ix_Installation_DeleteAt on Installation(DeleteAt)`)
+			if err != nil {
+				return err
+			}
+			_, err = e.Exec(`CREATE INDEX ix_Installation_State on Installation(State)`)
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
 	}},
 	{semver.MustParse("0.41.0"), semver.MustParse("0.42.0"), func(e execer) error {
 		fieldType := "JSONB"
+		if e.DriverName() == driverSqlite {
+			fieldType = "TEXT"
+		}
 
 		_, err := e.Exec(fmt.Sprintf(`ALTER TABLE Webhooks ADD COLUMN Headers %s NULL;`, fieldType))
 		if err != nil {
