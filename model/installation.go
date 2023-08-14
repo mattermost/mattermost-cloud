ace92fe3f3f202468a7c63c357c84f1c27b0decc
Stavros Foteinopoulos <stafot@gmail.com>
2023-08-14 17:48:35 +0300

    Implement NGINX IP Filtering (#951)
    * draft pr to check unitests with Gabe
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    * refactor and fixes
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    * review fixes
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    * Remove accidentally added orig file
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    * self-review fixes
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    * Refactor/Simplification upon review
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>
    
    ---------
    
    Signed-off-by: Stavros Foteinopoulos <stafot@gmail.com>

--- model/installation.go
+++ model/installation.go
@@ -38,6 +38,7 @@ type Installation struct {
 	ExternalDatabaseConfig     *ExternalDatabaseConfig     `json:"ExternalDatabaseConfig,omitempty"`
 	Filestore                  string
 	License                    string
+	AllowedIPRanges            string
 	MattermostEnv              EnvVarMap
 	PriorityEnv                EnvVarMap
 	Size                       string
