f7d131d2480c7249f88d2221b4302661c5f5cbcf
Nick Misasi <nick.misasi@mattermost.com>
2023-11-02 16:47:31 -0400

    Fix tests, linter

--- internal/provisioner/cluster_installation_provisioner_test.go
+++ internal/provisioner/cluster_installation_provisioner_test.go
@@ -11,23 +11,23 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestaddSourceRangeWhitelistToAnnotations(t *testing.T) {
+func TestAddSourceRangeWhitelistToAnnotations(t *testing.T) {
 	t.Run("nil allowed ranges, blank internal ranges", func(t *testing.T) {
 		annotations := getIngressAnnotations()
-		addSourceRangeWhitelistToAnnotations(annotations, nil, "")
+		addSourceRangeWhitelistToAnnotations(annotations, nil, []string{""})
 		require.Equal(t, getIngressAnnotations(), annotations)
 	})
 
 	t.Run("nil allowed ranges, internal ranges", func(t *testing.T) {
 		annotations := getIngressAnnotations()
-		addSourceRangeWhitelistToAnnotations(annotations, nil, "2.2.2.2/24")
+		addSourceRangeWhitelistToAnnotations(annotations, nil, []string{"2.2.2.2/24"})
 		require.Equal(t, getIngressAnnotations(), annotations)
 	})
 
 	t.Run("allowed ranges, blank internal ranges", func(t *testing.T) {
 		annotations := getIngressAnnotations()
 		allowedRanges := &model.AllowedIPRanges{{CIDRBlock: "1.1.1.1/24"}}
-		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, "")
+		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, []string{""})
 		require.Equal(t, "1.1.1.1/24", annotations.WhitelistSourceRange)
 		require.Equal(t, annotations, getIngressAnnotations())
 	})
@@ -35,7 +35,7 @@ func TestaddSourceRangeWhitelistToAnnotations(t *testing.T) {
 	t.Run("allowed ranges, blank internal ranges", func(t *testing.T) {
 		annotations := getIngressAnnotations()
 		allowedRanges := &model.AllowedIPRanges{{CIDRBlock: "1.1.1.1/24"}}
-		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, "2.2.2.2/24")
+		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, []string{"2.2.2.2/24"})
 		require.Equal(t, "1.1.1.1/24,2.2.2.2/24", annotations.WhitelistSourceRange)
 		require.Equal(t, annotations, getIngressAnnotations())
 	})
@@ -46,7 +46,7 @@ func TestaddSourceRangeWhitelistToAnnotations(t *testing.T) {
 			{CIDRBlock: "1.1.1.1/24"},
 			{CIDRBlock: "1.1.1.2/24"},
 		}
-		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, "2.2.2.2/24,2.2.2.3/24")
+		addSourceRangeWhitelistToAnnotations(annotations, allowedRanges, []string{"2.2.2.2/24,2.2.2.3/24"})
 		require.Equal(t, "1.1.1.1/24,1.1.1.2/24,2.2.2.2/24,2.2.2.3/24", annotations.WhitelistSourceRange)
 		require.Equal(t, annotations, getIngressAnnotations())
 	})
