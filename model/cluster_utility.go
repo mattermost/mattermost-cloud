a92d0da4df21c981df94adb60289160a71345072
Stylianos Rigas <stylianosrigas@gmail.com>
2023-12-21 13:33:44 +0200

    CLD-6722 Upgrade Thanos to 0.33.0 and helm version 12.20.0 (#1000)
    Co-authored-by: Stylianos Rigas <stylianosrigas@Stylianoss-MacBook-Pro-Work.local>

--- model/cluster_utility.go
+++ model/cluster_utility.go
@@ -66,7 +66,7 @@ var DefaultUtilityVersions map[string]*HelmUtilityVersion = map[string]*HelmUtil
 	// PrometheusOperatorCanonicalName defines the default version and values path for the Helm chart
 	PrometheusOperatorCanonicalName: {Chart: "54.2.2", ValuesPath: ""},
 	// ThanosCanonicalName defines the default version and values path for the Helm chart
-	ThanosCanonicalName: {Chart: "12.10.1", ValuesPath: ""},
+	ThanosCanonicalName: {Chart: "12.20.0", ValuesPath: ""},
 	// NginxCanonicalName defines the default version and values path for the Helm chart
 	NginxCanonicalName: {Chart: "4.7.1", ValuesPath: ""},
 	// NginxInternalCanonicalName defines the default version and values path for the Helm chart
