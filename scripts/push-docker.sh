2fbcecdec564210ac54479c33b8554ba0d041887
Stavros Foteinopoulos <stafot@gmail.com>
2024-01-11 14:41:50 +0200

    Revert "Support ARM builds"

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -5,23 +5,27 @@
 
 set -euox
 
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
 
 if [ "$TAG" = "" ]; then
     echo "TAG was not provided"
     exit 1
 fi
 
-
 echo "Tagging images with SHA $TAG"
 
+docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:$TAG
 docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:$TAG
 
+docker push mattermost/mattermost-cloud:$TAG
 docker push mattermost/mattermost-cloud-e2e:$TAG
 
 if [ "$REF_NAME" = "master" ] || [ "$REF_NAME" = "main" ]; then
     echo "Tagging images with 'latest' tag"
 
+    docker tag mattermost/mattermost-cloud:test mattermost/mattermost-cloud:latest
     docker tag mattermost/mattermost-cloud-e2e:test mattermost/mattermost-cloud-e2e:latest
+
+    docker push mattermost/mattermost-cloud:latest
     docker push mattermost/mattermost-cloud-e2e:latest
 fi
