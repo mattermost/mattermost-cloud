f23ad5bcfc22cd8cf85e184e3c1b39059a3d5173
Andre Leite <andrleite@gmail.com>
2024-02-07 15:45:01 -0300

    [CLD-5708]sync gitops app before remove it from argo

--- internal/provisioner/kops_provisioner.go
+++ internal/provisioner/kops_provisioner.go
@@ -918,6 +918,13 @@ func (provisioner *KopsProvisioner) DeleteCluster(cluster *model.Cluster) (bool,
 		return false, errors.Wrap(err, "failed to remove cluster from Argocd")
 	}
 
+	// Sync the gitops-sre-dev application to remove it from argocd
+	appName := "gitops-sre-" + provisioner.awsClient.GetCloudEnvironmentName()
+	_, err = argocdClient.SyncApplication(appName)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to sync argocd application")
+	}
+
 	logger.Info("Successfully deleted Kops cluster")
 
 	return true, nil
