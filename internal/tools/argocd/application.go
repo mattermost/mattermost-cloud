d954dee8c47b241e6b1953c6efec03e559d1d797
Andre Leite <andrleite@gmail.com>
2024-06-19 17:28:31 -0300

    Update internal/tools/argocd/application.go
    Co-authored-by: Gabe Jackson <3694686+gabrieljackson@users.noreply.github.com>

--- internal/tools/argocd/application.go
+++ internal/tools/argocd/application.go
@@ -57,8 +57,7 @@ func (c *ApiClient) WaitForAppHealthy(appName string, timeout time.Duration) err
 
 		// Check for timeout
 		if time.Since(startTime) >= timeout {
-			err = errors.New("timed out waiting for application to be healthy")
-			return err
+			return errors.New("timed out waiting for application to be healthy")
 		}
 
 		//Add a small delay to reduce CPU usage and avoid too_many_pings error.
