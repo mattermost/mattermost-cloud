20a4242bc3e649c7899b0952224b057b354ce10b
Andre Leite <andrleite@gmail.com>
2025-04-01 13:20:12 -0300

    bump installation version (#1104)
    * bump installation version
    
    * remove version to always get latest

--- e2e/pkg/installation_builder.go
+++ e2e/pkg/installation_builder.go
@@ -18,7 +18,6 @@ type InstallationBuilder struct {
 func NewInstallationBuilderWithDefaults() *InstallationBuilder {
 	return &InstallationBuilder{request: &model.CreateInstallationRequest{
 		OwnerID:   "e2e-test",
-		Version:   "7.9.1",
 		Image:     "mattermost/mattermost-enterprise-edition",
 		Size:      "1000users",
 		Affinity:  "multitenant",
